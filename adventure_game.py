bec426f80267fc0f6348a87ea63b92f50f795657
Eshita Islam <islam.eshita@hotmail.com>
2019-06-21 18:38:22 -0700

    Added functions and refactored the code

--- adventure_game.py
+++ adventure_game.py
@@ -1,24 +1,41 @@
 import time
+      
 
-print("You find yourself in a dark dungeon.")
-time.sleep(2)
-print("In front of you are two passageways.")
-time.sleep(2)
+def print_pause(message):
+    print(message)
+    time.sleep(2)
 
-print("")
-print("Enter 1 to knock on the door of the house.")
-print("Enter 2 to peer into the cave.")
-choice = input("(Please enter 1 or 2.)\n")
-valid_choice = choice == "1" or choice == "2"
+def intro():
+    print_pause("You find yourself in a dark dungeon.")
+    print_pause("In front of you are two passageways.")
 
-while(not valid_choice):
-    if choice == "1":
-        print("User chose 1")
-    elif choice == "2":
-        print("User chose 2")
-    else:
+def get_choice():    
+    valid_choice = False
+    choice = ""
+    while not valid_choice:
         print("")
         print("Enter 1 to knock on the door of the house.")
         print("Enter 2 to peer into the cave.")
         choice = input("(Please enter 1 or 2.)\n")
-        valid_choice = choice == "1" or choice == "2"
+        valid_choice = (choice == "1" or choice == "2")
+
+    if choice == "1":
+        house()
+    elif choice == "2":
+        cave()
+
+def house():
+    # Things that happen to the player in the house
+    print("Player inside the house")
+
+def cave():
+    # Things that happen to the player in the cave
+    print("Player In the cave")
+
+def field():
+    print("Player In the field")
+
+
+
+intro()
+get_choice()
