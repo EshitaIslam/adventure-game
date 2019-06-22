9d0ef2729078967a11880206150a956853562bb2
Eshita Islam <islam.eshita@hotmail.com>
2019-06-21 18:22:45 -0700

    Added code to give player some choices

--- adventure_game.py
+++ adventure_game.py
@@ -1,1 +1,16 @@
+import time
+
 print("You find yourself in a dark dungeon.")
+time.sleep(2)
+print("In front of you are two passageways.")
+time.sleep(2)
+
+print("")
+print("Enter 1 to knock on the door of the house.")
+print("Enter 2 to peer into the cave.")
+choice = input("(Please enter 1 or 2.)\n")
+
+if choice == "1":
+    print("You chose 1")
+elif choice == "2":
+    print("You chose 1")
