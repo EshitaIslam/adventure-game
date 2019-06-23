6435028d63250b28c02b34deee12c2007c20ae6f
Eshita Islam <islam.eshita@hotmail.com>
2019-06-22 22:47:56 -0700

    Added code for things the player can do in the house.

--- adventure_game.py
+++ adventure_game.py
@@ -11,9 +11,10 @@ def get_random_enemy():
 
 def intro(enemy):
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause(f"Rumor has it that a {enemy} is somewhere around here, and has been terrifying the nearby willage")
-    print_pause("In front of you is a house")
-    print_pause("In your right is a dark cave")
+    print_pause(f"Rumor has it that a {enemy} is somewhere around here, and has been terrifying the nearby village.")
+    print_pause("In front of you is a house.")
+    print_pause("In your right is a dark cave.")
+    print_pause("In your hand you hold your trusty (but not very effective) dagger.")
 
 def get_choice(choices):        
     
@@ -24,15 +25,36 @@ def get_choice(choices):
 
     selected_choice = ""
     while not (selected_choice == "1" or selected_choice == "2"):       
-        selected_choice = input("(Please enter 1 or 2.)\n")
+        selected_choice = input("Please enter 1 or 2: ")
         
     return selected_choice
     
-def house():
-    # Things that happen to the player in the house
-    print("Player inside the house")
+def house(enemy, weapons_picked, special_weapon):    
+    print_pause("You approach the door of the house.")
+    print_pause(f"You are about to knock when the door opens and out steps a {enemy}")
+    print_pause(f"Eep! This is the {enemy}'s house!")
+    print_pause(f"The {enemy} attacks you!")
 
-def cave():
+    if special_weapon in weapons_picked:
+        print_pause(f"But the {enemy} takes one look at your shiny new toy and runs away!")
+        print_pause(f"You have rid the town of the {enemy}. You are victorious!")
+    else:
+        print_pause("You feel a bit under-prepared for this, what with only having a tiny dagger.")
+        choice = get_choice(["Enter 1 to fight", "Enter 2 to run away"])
+        if choice == "1":
+            print_pause("You do your best...")
+            print_pause(f"But your dagger is no match for the {enemy}.")
+            print_pause("You have been defeated!")
+        elif choice == "2":
+            print_pause("You run back into the field. Luckily, you don't seem to have been followed.")
+            print_pause("")
+            choice = get_choice(["Enter 1 to knock on the door of the house.","Enter 2 to peer into the cave."])
+            if choice == "1":
+                house(enemy, weapons_picked, special_weapon)
+            elif choice == "2":
+                cave(weapons_picked)
+
+def cave(weapons_picked):
     # Things that happen to the player in the cave
     print("Player In the cave")
 
@@ -41,12 +63,14 @@ def field():
 
 def play_game():
     enemy = get_random_enemy()
+    weapons_picked = []
+    special_weapon = "Sword of Ogorth"
     
     intro(enemy)
     choice = get_choice(["Enter 1 to knock on the door of the house.","Enter 2 to peer into the cave."])
     if choice == "1":
-        house()
+        house(enemy, weapons_picked, special_weapon)
     elif choice == "2":
-        cave()
+        cave(weapons_picked)
 
 play_game()
